4ca46093b33d11205d7804333ea2608856e4eddd
karanDevX11 <bistakaran89@gmail.com>
2025-01-07 23:12:28 +0545

    Update add and display post

--- myblog/routes.py
+++ myblog/routes.py
@@ -8,25 +8,10 @@ from myblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
-
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
